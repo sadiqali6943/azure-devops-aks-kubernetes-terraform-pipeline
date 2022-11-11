a56c8e9e0d643745d67676cc022c1e03197dfb44
sadiqali6943 <116359070+sadiqali6943@users.noreply.github.com>
2022-11-11 15:27:47 +0530

    Update 02-variables.tf

--- terraform-manifests/02-variables.tf
+++ terraform-manifests/02-variables.tf
@@ -16,7 +16,7 @@ variable "resource_group_name" {
 variable "environment" {
   type = string  
   description = "This variable defines the Environment"  
-  default = "UAT"
+  default = "dev"
 }
 
 
